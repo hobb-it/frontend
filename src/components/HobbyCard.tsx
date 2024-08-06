e6ee49477df0f358ca23e174eb73fcfa959c395b
Davide <davise.mauri998@hotmail.it>
2024-08-06 15:12:02 +0200

    Diplayed hobbycard title

--- src/components/HobbyCard.tsx
+++ src/components/HobbyCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/hobbyCard.css"
+import "../styles/hobbyCard.css";
 
 interface Props {
   hobbyCard: any;
@@ -15,18 +15,29 @@ function HobbyCard({ hobbyCard, buttonText, buttonLink, hideFields }: Props) {
       <div className="card shadow-lg hobby-card">
         <div className="card-body card-content">
           <h4 className="card-title text-left mb-3 border-bottom pb-2">
-            {hobbyCard.categoryName}
+            {hobbyCard.title}
           </h4>
 
-          {!hideFields && (
-            <>
-              <div className="row yellow-100">
-                <h5 className="col-12 font-weight-bold text-left">Pro User</h5>
-                <span className="col-12 text-left">{hobbyCard.username}</span>
+          <div className="row">
+            {!hideFields && (
+              <div className="col-12 col-md-6 container-fluid">
+                <div className="row yellow-100">
+                  <h5 className="col-12font-weight-bold text-left">Pro User</h5>
+                  <span className="col-12 text-left">{hobbyCard.username}</span>
+                </div>
               </div>
-              <hr />
-            </>
-          )}
+            )}
+
+            <div className="col-12 col-md-6 container-fluid">
+              <div className="row yellow-100">
+                <h5 className="col-12 font-weight-bold text-left">Category</h5>
+                <span className="col-12 text-left">
+                  {hobbyCard.categoryName}
+                </span>
+              </div>
+            </div>
+          </div>
+          <hr />
 
           <div className="row text-left">
             <h5 className="col-12 font-weight-bold text-left fs-5">
